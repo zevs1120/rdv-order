8862029994d7fa2d39bf2935d8a5a78548beee49
zevs1120 <zevs1120@gmail.com>
2026-03-05 00:24:16 +0800

    Revert "Replace PWA home screen icons with fbLOGO assets"
    This reverts commit 4805e04ddad03f369e4f6bd6f85594dcf52f85e5.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,8 @@ export const metadata: Metadata = {
     statusBarStyle: "default"
   },
   icons: {
-    icon: [
-      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
-    ],
-    apple: "/icons/apple-touch-icon.png"
+    icon: "/icons/icon.svg",
+    apple: "/icons/icon.svg"
   }
 };
 
