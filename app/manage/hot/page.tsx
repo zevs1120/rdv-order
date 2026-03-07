c8ff091aae857e347165a5032fc4cf2409fa8e0c
zevs1120 <zevs1120@gmail.com>
2026-03-08 00:25:44 +0800

    perf(C): mark dynamic manage wrappers as client entries

--- app/manage/hot/page.tsx
+++ app/manage/hot/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const ManageHotClientPage = dynamic(() => import("./page-client"), {
--- app/manage/income/page.tsx
+++ app/manage/income/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 const ManageIncomeClientPage = dynamic(() => import("./page-client"), {
